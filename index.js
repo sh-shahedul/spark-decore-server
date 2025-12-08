9e904b513efc6139a027a9a1a69aec16cce28015
Shahedul Hoque <217447shah@gmsil.com>
2025-12-09 02:43:36 +0600

    implent booking post api

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wbmojlp.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,7 +28,7 @@ async function run() {
         const db = client.db("spark_decore");
         const serviceCollection = db.collection("services");
         const userCollection = db.collection("users");
-        
+        const bookingCollection = db.collection("bookings")
         
   
         //  user releted api 
@@ -64,7 +64,27 @@ async function run() {
              const result= await cursor.toArray()
              res.send(result)
          })
+
+        //  singel service for details 
+         app.get('/services/:id',async(req,res)=>{
+          const id = req.params.id
+          const query = {_id : new ObjectId(id)}
+          const result = await serviceCollection.findOne(query)
+          res.send(result)
+         })
             
+      //  booking related api 
+        app.post('/bookings',async(req,res)=>{
+          const newBook = req.body
+          const result = await bookingCollection.insertOne(newBook)
+          res.send(result)
+        })
+
+
+
+
+
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
